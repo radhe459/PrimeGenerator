2ffe8b178a43a90b3aaffe2fb53b1410beb529dd
Radhe Shyam Chaubey <informativersc1234@gmail.com>
2023-03-18 23:37:33 +0530

    prime generator completed

---
+++ Prime.js
@@ -0,0 +1,41 @@
+
+function res(str, a) {
+    str = str + " " + a;
+    return str
+}
+function Printprime() {
+    let a = document.getElementById("first").value;
+    let b = document.getElementById("last").value;
+    a = Number.parseInt(a)
+    b = Number.parseInt(b)
+    let str = "";
+    let count = 0;
+    if (a == 2 && a > 1)
+        str += 2;
+    if (a < 1 || b < a)
+        str = "Enter valid start and end"
+    for (let i = a; i <= b; i++) {
+        let k = 0;
+        for (let j = 2; j <= Math.floor(Math.sqrt(i)); j++) {
+            if (i % j == 0) {
+                k = 1;
+                break;
+            }
+        }
+        if (k == 0) {
+            str = res(str, i);
+            count++;
+        }
+    }
+    document.getElementById("result").innerHTML = str;
+    document.getElementById("count").innerHTML = count;
+}
+
+function reset() {
+    document.getElementById("first").value = "";
+    document.getElementById("last").value = "";
+    document.getElementById("result").value = "";
+    document.getElementById("count").value = "";
+}
+
+
